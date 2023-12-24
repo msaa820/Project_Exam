99fadd03df1b7231343b8db0093b9585fe946d15
msaa820 <111208239+msaa820@users.noreply.github.com>
2023-12-24 07:03:10 -0800

    Update Project.c

--- Project.c
+++ Project.c
@@ -23,30 +23,37 @@ int main()
     j = 0;
     count = 0;
 
+    FILE *file = fopen("library.txt", "a");
+    if (file == NULL)
+    {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
     while (1)
     {
-        printf("===============================\n");
-        printf("|| Library System Management ||\n");
-        printf("===============================\n");
-        printf("  1 : Add Book details\n ");
-        printf(" 2 : Display the list of books and its details\n ");
-        printf(" 3 : Display the total numbers of books in the library\n ");
-        printf(" 4 : Exit\n\n");
-        printf(" Enter the number: ");
-        scanf(" %d", &j);
+        printf("\n\n1 : Add Book details\n");
+        printf("2 : Display the list of books and its details\n");
+        printf("3 : Display the total numbers of books in the library\n");
+        printf("4 : Save And Exit \n\n");
+        printf("Enter the number: ");
+        scanf("%d", &j);
 
         switch (j)
         {
         case 1:
-            printf(" \nYou can add the details of the book ");
-            printf(" \nEnter the book name: ");
-            scanf(" %s", lib[i].book_name);
-            printf(" \nEnter the author name: ");
-            scanf(" %s", lib[i].author_name);
-            printf(" \nEnter the number of pages: ");
-            scanf(" %d", &lib[i].no_of_pages);
-            printf(" \nEnter the cost of the book: ");
-            scanf(" %f", &lib[i].cost);
+            printf("\nYou can add the details of the book\n");
+            printf("Enter the book name: ");
+            scanf("%s", lib[i].book_name);
+            printf("Enter the author name: ");
+            scanf("%s", lib[i].author_name);
+            printf("Enter the number of pages: ");
+            scanf("%d", &lib[i].no_of_pages);
+            printf("Enter the cost of the book: ");
+            scanf("%f", &lib[i].cost);
+
+            fprintf(file, "The Book Name : %s\nThe outhor Name : %s\nThe Number of page : %d\nBook cost : %.2f\n===================================\n", lib[i].book_name, lib[i].author_name, lib[i].no_of_pages, lib[i].cost);
+
             count = count + 1;
             i = i + 1;
             break;
@@ -65,21 +72,22 @@ int main()
                     printf(" \nThe name of the book is: %s ", lib[i].book_name);
                     printf(" \nThe name of the author is: %s ", lib[i].author_name);
                     printf(" \nThe number of pages are: %d ", lib[i].no_of_pages);
-                    printf(" \nThe cost of the book is: %f\n\n ", lib[i].cost);
+                    printf(" \nThe cost of the book is: %f\n\n", lib[i].cost);
                     printf("=======================================");
                 }
             }
             break;
 
         case 3:
-            printf(" \nTotal number of books in the library are: %d\n\n ", count);
+            printf("\nTotal number of books in the library are: %d\n\n", count);
             break;
 
         case 4:
-            exit(0);
+            fclose(file);
+            return 0;
 
         default:
-            printf(" \nInvalid number entered\n\n ");
+            printf("\nInvalid number entered\n\n");
         }
     }
 }
